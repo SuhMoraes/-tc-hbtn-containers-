f65dd2ad688ca05fb579534edde82d104026513d
Suhailah de Moraes <68478385+SuhMoraes@users.noreply.github.com>
2022-05-04 16:07:06 -0300

    Add files via upload

---
+++ 0x03/docker/MessageResource.java
@@ -0,0 +1,15 @@
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value="/messages")
+public class MessageResource {
+
+    @GetMapping("/simpleMessageWelcome")
+    public String simpleMessageWelcome() {
+        var hostName = System.getenv("HOSTNAME");
+        return "environment: " + hostName;
+    }
+}
+
